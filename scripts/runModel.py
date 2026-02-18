31c5d545e27572cd13716b56a1d8558251e8bcfb
RaifCos <Raifcostellobusiness@gmail.com>
2026-02-18 16:10:07 +0000

    Change return format for Model

--- scripts/runModel.py
+++ scripts/runModel.py
@@ -1,7 +1,6 @@
-import sys
 import json
 from chord_cnn_lstm import chord_recognition
 
 def main(audioPath):
     output = chord_recognition.main(audioPath)
-    print(json.dumps(output))
+    return json.dumps(output)
