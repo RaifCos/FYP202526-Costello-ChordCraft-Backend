715a486fdb679efb7ef5f454abca6b2d80096e23
Raif Costello <128611497+RaifCos@users.noreply.github.com>
2026-02-26 14:40:12 +0000

    Add missing String Formatting

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ async def runChordExtraction(file: UploadFile = File(...)):
     print("API Endpoint reached.")
     ext = os.path.splitext(file.filename)[1].lower()
     if ext != ".mp3":
-        raise HTTPException(status_code=400, detail="Invalid file type {ext}")
+        raise HTTPException(status_code=400, detail=f"Invalid file type {ext}")
 
     tempFile = None
     try:
